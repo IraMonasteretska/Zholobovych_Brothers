9b111f9a79974d1293ca98bc8de7c456ba0c2350
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2022-10-06 11:03:20 +0300

    Add files via upload
    add other pages

--- js/main.js
+++ js/main.js
@@ -22,14 +22,11 @@ $(document).ready(function () {
         $('.header__menu').toggleClass('active');
     });
 
-    
+
 
     // heroimage slider
     var swiper = new Swiper(".heroimage__slider", {
         effect: "fade",
-        // fadeEffect: {
-        //     crossFade: true
-        // },
         autoplay: {
             delay: 800,
         },
@@ -42,7 +39,7 @@ $(document).ready(function () {
         },
     });
 
-    // ------ accordeon ------- //
+    // accordeon 
 
     $('.process__acordeon-header').click(function () {
         $(this).toggleClass('active');
@@ -86,7 +83,7 @@ $(document).ready(function () {
                 slidesPerView: 2,
                 slidesPerGroup: 2,
             }
-          }
+        }
     });
     var swiper = new Swiper(".gallery__slider-bottom", {
         slidesPerView: 3,
@@ -112,13 +109,88 @@ $(document).ready(function () {
             1200: {
                 spaceBetween: 32,
             }
-          }
+        }
     });
 
     // gallery popup
     Fancybox.bind('[data-fancybox="gallery a"]', {
         infinite: false
-      });
+    });
+
+    //  slider product image
+    var smallImage = new Swiper(".smallImage", {
+        slidesPerView: 4,
+        direction: "vertical"
+    });
+    var bigImage = new Swiper(".bigImage", {
+        effect: "fade",
+        thumbs: {
+            swiper: smallImage,
+        },
+    });
+
+    //  popup product card
+    $('.popupbtn').on('click', function (e) {
+        e.preventDefault();
+        $('.popup').addClass('active');
+    });
+    $('.popup__close').on('click', function () {
+        $('.popup').removeClass('active');
+    });
+
+    // counter
+    $('.minus').click(function () {
+        var $input = $('.product__form-counter').find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        summ();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $('.product__form-counter').find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        summ();
+        return false;
+    });
+
+
+    // calculate 
+    var material = 1000,
+        color = 500,
+        uzor = 0,
+        amount = 0;
+
+    $("input[name='material']").change(function () {
+            material = $(this).data('number');
+            if($(this).hasClass('uncommon')){
+                $(".entry").addClass('disabled');
+            } else {
+                $(".entry").removeClass('disabled');
+            }
+        summ();
+    });
+
+    $("input[name='color']").change(function () {
+            color = $(this).data('number');
+        summ();
+    });
+
+    $("input[name='uzor']").change(function () {
+            uzor = $(this).data('number');
+        summ();
+    });
+
+
+    function summ() {
+        material = Number(material);
+        color = Number(color);
+        uzor = Number(uzor);
+        amount = Number($('.product__form-counter').find('input').val());
+        $(".price p").text((material + color + uzor) * amount);
+    }
 
 
 });
